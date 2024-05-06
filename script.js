55b7355b8acf495e193c917e2df353a2c5980467
Hans Pinckaers <hans.pinckaers@gmail.com>
2024-05-06 18:56:30 +0200

    new

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 $(document).ready( function () {
     var table = $('#myTable').DataTable({
-        "paging": false,
+        stateSave: true,
+        "paging": true,
+        "pageLength": 50,
         scrollX: true,
         searchPanes: {
             cascadePanes: true,
-            layout: 'columns-3',
+            layout: 'columns-1',
             collapse: false,
             orderable: false,
             dtOpts: {
@@ -12,14 +14,51 @@ $(document).ready( function () {
             }
         },
         dom: 'Plfrtip',
-        columnDefs: [{
+        columnDefs: [
+            // { "width": "5%", "targets": 0 },
+            // { "width": "10%", "targets": 1 },
+            // { "width": "50%", "targets": 2 },
+            // { "width": "10%", "targets": 3 },
+            // { "width": "20%", "targets": 4 },
+            {
                 searchPanes: {
                     show: false
                 },
-                targets: [5, 3]
+                // targets: [5, 3]
             },
+            {
+                targets: [-1],
+                visible: false
+            }
         ],
         autoWidth: false,
+        createdRow: function (row, data, dataIndex) {
+            var linkHtml = data[data.length - 1];
+            var url = $(linkHtml).attr('href');
+            $(row).attr('data-url', url);
+        }
     });
     table.column( 0 ).visible( false );
+    table.column( -1 ).visible( false );
+
+    document
+        .querySelector('div.dtsp-verticalPanes')
+        .appendChild(table.searchPanes.container().get(0));
+
+    $("#myTable").on('click', 'tr', function (e) {
+        var url = $(this).data('url');
+        if (url) {
+            window.location.href = url;
+        } 
+    });
+    var hamburgerBtn = document.getElementById('hamburgerBtn');
+    var verticalContainer = document.querySelector('.dtsp-verticalPanes');
+
+    hamburgerBtn.addEventListener('click', function() {
+        if (verticalContainer.style.display === 'none') {
+        verticalContainer.style.display = 'flex';
+        } else {
+        verticalContainer.style.display = 'none';
+        }
+    });
 } );
